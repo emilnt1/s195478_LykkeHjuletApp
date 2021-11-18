4ba42e3037ceaced7cd5be6b5f84e9e34a80534e
Emil Trangeled <emilbfh@hotmail.com>
2021-11-18 18:54:31 +0100

    Messed around with fragments and screens

--- app/src/main/java/com/example/s195478_lykkehjuletapp/MainActivity.kt
+++ app/src/main/java/com/example/s195478_lykkehjuletapp/MainActivity.kt
@@ -7,5 +7,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val startFragment = StartFragment()
+        val wordFragment = wordsFragment()
+
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragScreen, startFragment)
+            commit()
+        }
+
     }
 }
